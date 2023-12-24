712437ac3c8db36bd5a61a0950bf1f024026130d
HasiBul Islam ShanTo <hasib@nybsys.com>
2023-12-24 16:32:55 +0600

    remove port

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -18,7 +18,7 @@ namespace Walkthrough.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet(Name = "Get")]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
